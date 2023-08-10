7606b01629e016382f57cf000c384d3c9113825f
Shivam Nahar <shivamnahar09@gmail.com>
2023-08-09 23:14:09 -0400

    implemented the review controller class

--- movie/src/main/java/dev/shivam/movie/ReviewController.java
+++ movie/src/main/java/dev/shivam/movie/ReviewController.java
@@ -1,5 +1,22 @@
 package dev.shivam.movie;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
 public class ReviewController {
+
+    @Autowired
+    private ReviewService reviewService;
+@PostMapping
+    public ResponseEntity<Review> createReview(@RequestBody Map<String,String> payload)
+    {
+        return new ResponseEntity<>(reviewService.createReview(payload.get("reviewBody"),payload.get("imdbId")), HttpStatus.CREATED);
+    }
 }
