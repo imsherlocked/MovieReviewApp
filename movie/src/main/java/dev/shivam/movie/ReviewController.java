8e9fdd5a6920f9485ccd3e68967e626c60bec699
Shivam Nahar <shivamnahar09@gmail.com>
2023-08-09 23:16:29 -0400

    implemeted api to create movie review

--- movie/src/main/java/dev/shivam/movie/ReviewController.java
+++ movie/src/main/java/dev/shivam/movie/ReviewController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
--- movie/src/main/java/dev/shivam/movie/ReviewRepository.java
+++ movie/src/main/java/dev/shivam/movie/ReviewRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+    //add methods.
 }
