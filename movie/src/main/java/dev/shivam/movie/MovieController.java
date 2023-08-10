816e6288866e775213e85e1aec4b52b696e37cd9
Shivam Nahar <shivamnahar09@gmail.com>
2023-08-09 22:43:53 -0400

    Added Api's

--- movie/src/main/java/dev/shivam/movie/MovieController.java
+++ movie/src/main/java/dev/shivam/movie/MovieController.java
@@ -1,18 +1,31 @@
 package dev.shivam.movie;
 
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
 
-    
+    @Autowired
+    private MovieService movieService;
     @GetMapping
-    public ResponseEntity<String> getMovies(){
-        return new ResponseEntity<String>("All Movies", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> allMovies(){
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(),HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable String imdbId)
+    {
+        return new ResponseEntity<>(movieService.getMovieById(imdbId),HttpStatus.OK);
     }
 }
--- movie/src/main/java/dev/shivam/movie/MovieService.java
+++ movie/src/main/java/dev/shivam/movie/MovieService.java
@@ -1,20 +1,27 @@
 package dev.shivam.movie;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
 
-    @Autowired
-    private MovieController movieController;
+
+   // private MovieController movieController;
     @Autowired
     private MovieRepository movieRepository;
 
     public List<Movie> allMovies(){
 
-        return null;
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> getMovieById(String imdbId)
+    {
+        return movieRepository.getMovieByimdbId(imdbId);
     }
 }
