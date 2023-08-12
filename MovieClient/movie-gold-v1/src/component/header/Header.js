e72a2d257dd8ac651dd0880168dbf3e518bd4aae
Shivam Nahar <shivamnahar09@gmail.com>
2023-08-11 23:42:06 -0400

    added the header component

---
+++ MovieClient/movie-gold-v1/src/component/header/Header.js
@@ -0,0 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVideoSlash } from "@fortawesome/free-solid-svg-icons";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  returnimport(<div>Header</div>);
+};
+
+export default Header;
