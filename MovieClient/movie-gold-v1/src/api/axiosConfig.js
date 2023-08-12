53315a2a70682c376006013fef5ffe07823e8f22
Shivam Nahar <shivamnahar09@gmail.com>
2023-08-11 22:25:49 -0400

    removed ngrok

--- MovieClient/movie-gold-v1/src/api/axiosConfig.js
+++ MovieClient/movie-gold-v1/src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   //2TrfdEq1Eq5uqsZU2R3p2CwwSpq_4xkTZnx3Am28TddUNiBvw- NGROK AUTH TOKEN
-  baseURL: "https://7e9e-67-21-186-132.ngrok.io/", //"https://9c96-103-106-239-104.ap.ngrok.io",
+  baseURL: "http://localhost:8080",
 
   headers: { "ngrok-skip-browser-warning": "true" },
 });
