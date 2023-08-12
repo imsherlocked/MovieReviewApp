c978e3d2ce2e771765bb033e4937004ce5c1412a
Shivam Nahar <shivamnahar09@gmail.com>
2023-08-11 23:54:57 -0400

    Updated header.js and app.js files

--- MovieClient/movie-gold-v1/src/App.js
+++ MovieClient/movie-gold-v1/src/App.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import Layout from "./component/Layout";
 import { Routes, Route } from "react-router-dom";
 import Home from "./component/home/Home";
+import Header from "./component/header/Header";
 
 function App() {
   const [movies, setMovies] = useState();
@@ -23,6 +24,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home movies={movies} />}></Route>
