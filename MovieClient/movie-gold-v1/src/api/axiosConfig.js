b6ee9c6a8de2ec56ea50bb846b3c4e95267abedf
Shivam Nahar <shivamnahar09@gmail.com>
2023-08-11 22:25:13 -0400

    Added cors

--- MovieClient/movie-gold-v1/src/api/axiosConfig.js
+++ MovieClient/movie-gold-v1/src/api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://f840-67-21-186-132.ngrok.io", //"https://9c96-103-106-239-104.ap.ngrok.io",
+  //2TrfdEq1Eq5uqsZU2R3p2CwwSpq_4xkTZnx3Am28TddUNiBvw- NGROK AUTH TOKEN
+  baseURL: "https://7e9e-67-21-186-132.ngrok.io/", //"https://9c96-103-106-239-104.ap.ngrok.io",
 
   headers: { "ngrok-skip-browser-warning": "true" },
 });
--- movie/src/main/java/dev/shivam/movie/MovieController.java
+++ movie/src/main/java/dev/shivam/movie/MovieController.java
@@ -9,8 +9,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "*")
+
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1/movies")
 public class MovieController {
 
