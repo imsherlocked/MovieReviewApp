8010f6bd3b93e599b2eb77860874d8cd969bce0b
Shivam Nahar <shivamnahar09@gmail.com>
2023-08-11 23:51:39 -0400

    integrated trailer functionality on home page

--- MovieClient/movie-gold-v1/src/component/hero/Hero.js
+++ MovieClient/movie-gold-v1/src/component/hero/Hero.js
@@ -1,6 +1,10 @@
 import "./Hero.css";
 import Carousel from "react-material-ui-carousel";
 import { Paper } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import { Link, useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 const Hero = ({ movies }) => {
   return (
@@ -20,6 +24,28 @@ const Hero = ({ movies }) => {
                     </div>
                     <div className="movie-title">
                       <h4>{movie.title}</h4>
+                    </div>
+                    <div className="movie-buttons-container">
+                      <Link
+                        to={`/Trailer/${movie.trailerLink.substring(
+                          movie.trailerLink.length - 11
+                        )}`}
+                      >
+                        <div className="play-button-icon-container">
+                          <FontAwesomeIcon
+                            className="play-button-icon"
+                            icon={faCirclePlay}
+                          />
+                        </div>
+                      </Link>
+                      <div className="movie-review-button-container">
+                        <Button
+                          variant="info"
+                          onClick={() => reviews(movie.imdbId)}
+                        >
+                          Reviews
+                        </Button>
+                      </div>
                     </div>
                   </div>
                 </div>
