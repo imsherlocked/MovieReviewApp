e2230587a042050709438efd224c04d3ede74f91
Shivam Nahar <shivamnahar09@gmail.com>
2023-08-11 22:12:22 -0400

    Added ngrok

--- MovieClient/movie-gold-v1/src/api/axiosConfig.js
+++ MovieClient/movie-gold-v1/src/api/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://9c96-103-106-239-104.ap.ngrok.io",
+  baseURL: "https://f840-67-21-186-132.ngrok.io", //"https://9c96-103-106-239-104.ap.ngrok.io",
+
   headers: { "ngrok-skip-browser-warning": "true" },
 });
