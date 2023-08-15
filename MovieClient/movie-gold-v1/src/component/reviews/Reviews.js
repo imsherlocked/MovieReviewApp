b914c65660ef5d5f904e05585fadc86f37b96566
Shivam Nahar <shivamnahar09@gmail.com>
2023-08-14 22:54:08 -0400

    added reviews folder

---
+++ MovieClient/movie-gold-v1/src/component/reviews/Reviews.js
@@ -0,0 +1,5 @@
+import { useEffect, useRef } from "react";
+import api from "../../api/axiosConfig";
+import { useParams } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import ReviewForm from "../reviewForm/ReviewForm";
